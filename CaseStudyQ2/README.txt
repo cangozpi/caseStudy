de64b928c578af04b5af3860f7cb2483f780b501
can <cangozpinar@gmail.com>
2021-03-20 17:59:49 +0300

    finished

--- CaseStudyQ2/README.txt
+++ CaseStudyQ2/README.txt
@@ -1,3 +1,7 @@
 run the code by typing node q2.js on the folder location.
-Output is written to bill.txt file.
-I 2 midterm exams today I am currently writing this code.
+The desired output is written to orderedBill.txt file.
+
+What it does is that, orderJson() fills an array named orderedBill() which is an array of JSON objects in response.json except the first element since it is an overall summary element.
+After calling orderedBill(), orderedBill array has in increasing order of the "y" leveles of the objects.
+Then, writeAfterOrdered() is called which iterates over the elements of the orderedBill and by checking the y coordinate difference between two adjacent elements and
+comparing it wrt offsetTolerance, decides whether to print on the same line or enter a newline and move on from there.
--- CaseStudyQ2/q2.js
+++ CaseStudyQ2/q2.js
@@ -1,40 +1,88 @@
 const fs = require('fs');
 
 let rawdata = fs.readFileSync('response.json');
-let bill = JSON.parse(rawdata);
-//console.log(bill[1].boundingPoly.vertices[0])
+//console.log(orderedBill[1].boundingPoly.vertices[0])
+let unorderedBill= JSON.parse(rawdata);
+var orderedBill = [];
 
-let output = "";
+//orders the unorderedBill and writes it on orderedBill
+let orderJson = () => {
 
-//iterate over the list and append description to output txt in a formatted way
-//use vertices to guide you
+    unorderedBill.splice(0,1); //remove the first element since its irrelevant
 
-let offsetTolerance = 10;//max offset btw same lines
-let lastYTop = bill[0].boundingPoly.vertices[0].y; 
+    while(unorderedBill.length > 1){
+        
+        
+        let currentminY = 10000000000; //represnet float(inf)
+        let currentMin;
+        let currentMinIndex = -1; // index of the min element
+        
+        for(var i = 1; i < unorderedBill.length; i++){
+            coordinates = unorderedBill[i].boundingPoly.vertices;
+            xy1 = coordinates[0]
+            xy2 = coordinates[1]
+            xy3 = coordinates[2]
+            xy4 = coordinates[3]
 
-for(var i = 1; i < bill.length; i++){
-    //parse the boundingPoly coordinates
-    coordinates = bill[i].boundingPoly.vertices;
-    xy1 = coordinates[0]
-    xy2 = coordinates[1]
-    xy3 = coordinates[2]
-    xy4 = coordinates[3]
+            if(currentminY > xy1.y){//if its above the currentMin
+                currentminY = xy1.y;
+                currentMinIndex = i;
+                currentMin = unorderedBill[i]; // set the json object to currentMin
+            }
 
-    if(Math.abs(lastYTop - xy1.y) < offsetTolerance){//if on the same line append to the same line
-        output += " " + bill[i].description;
+        }
 
-    }else{ //not on the same line
-        //break a line then, append and update lastYTop
-        output += "\n" + bill[i].description
-        lastYTop = xy1.y
+        //add the currentMin obj to orderedBill and remove it from the unorderedBill array
+        orderedBill.push(currentMin);
+        unorderedBill.splice(currentMinIndex,1);
+    
     }
     
+
 }
 
-console.log(bill[0].description)
+orderJson()
 
-//write to file
-fs.writeFile('bill.txt', output, function (err) {
-    if (err) return console.log(err);
-    console.log('JSON parsed and outputted written at bill.txt');
-  });
+
+//requires ordered List with respect to xy1.y coordinate in increasing order
+let writeAfterOrdered = () => {
+
+    let output = "";
+    //iterate over the list and append description to output txt in a formatted way
+    //use vertices to guide you
+
+    let offsetTolerance = 10;//max offset btw same lines
+    let lastYTop = orderedBill[0].boundingPoly.vertices[0].y; 
+
+
+    for(var i = 0; i < orderedBill.length; i++){
+        //parse the boundingPoly coordinates
+        coordinates = orderedBill[i].boundingPoly.vertices;
+        xy1 = coordinates[0]
+        xy2 = coordinates[1]
+        xy3 = coordinates[2]
+        xy4 = coordinates[3]
+    
+        if(Math.abs(lastYTop - xy1.y) < offsetTolerance){//if on the same line append to the same line
+            output += " " + orderedBill[i].description;
+    
+        }else{ //not on the same line
+            //break a line then, append and update lastYTop
+            output += "\n" + orderedBill[i].description
+            lastYTop = xy1.y
+        }
+        
+    }
+    
+    
+    //write to file
+    fs.writeFile('orderedBill.txt', output, function (err) {
+        if (err) return console.log(err);
+        console.log('JSON parsed and outputted written at orderedBill.txt');
+      });
+}
+
+
+
+
+writeAfterOrdered()
